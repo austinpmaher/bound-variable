02a8a6389a5af7b1344efcfc40d65329ae4ed8ae
Austin P. Maher <austinpmaher@yahoo.com>
2025-01-18 15:53:11 -0500

    Add vm.Opcode enum

--- uvm/vm.zig
+++ uvm/vm.zig
@@ -6,6 +6,23 @@ const ArrayList = std.ArrayList;
 
 const debug = true;
 
+const Opcode = enum(u4) {
+    ConditionalMove,
+    ArrayIndex,
+    ArrayAmendment,
+    Addition,
+    Multiplication,
+    Division,
+    NotAnd,
+    Halt,
+    Allocate,
+    Abandon,
+    Output,
+    Input,
+    LoadProgram,
+    LoadConstant,
+};
+
 const EMPTY_SLOT: []u32 = &[_]u32{};
 
 pub const VM = struct {
@@ -49,68 +66,82 @@ pub fn runVM(uvm: *VM) !void {
     while (true) {
         const instruction = uvm.memory.items[0][uvm.ip];
         uvm.ip += 1;
-        const opcode: u4 = @truncate(instruction >> 28);
+        const raw_opcode: u4 = @truncate(instruction >> 28);
+        const opcode: Opcode = @enumFromInt(raw_opcode);
         const a: u3 = @truncate((instruction >> 6) & 0x0007);
         const b: u3 = @truncate((instruction >> 3) & 0x0007);
         const c: u3 = @truncate(instruction & 0x0007);
 
         if (debug) {
-            std.debug.print("IP: {d}, Instruction: 0x{x:0>2}, Op: {d}, A: {d}, B: {d}, C: {d}\n", .{ uvm.ip - 1, instruction, opcode, a, b, c });
+            std.debug.print("IP: {d}, Instruction: 0x{x:0>2}, Op: {any}, A: {d}, B: {d}, C: {d}\n", .{ uvm.ip - 1, instruction, opcode, a, b, c });
             std.debug.print("Registers: {any}\n", .{uvm.reg});
         }
 
         switch (opcode) {
-            0 => {
+            .ConditionalMove => {
                 if (uvm.reg[c] != 0) uvm.reg[a] = uvm.reg[b];
             },
-            1 => {
+            .ArrayIndex => {
                 // case 1: reg[A] = arr(reg[B])[reg[C]]; break;
                 const ptr = try fetch_memory_ptr(uvm, uvm.reg[b]);
                 uvm.reg[a] = ptr[uvm.reg[c]];
             },
-            2 => {
+            .ArrayAmendment => {
                 // case 2: arr(reg[A])[reg[B]] = reg[C];
                 const ptr = try fetch_memory_ptr(uvm, uvm.reg[a]);
                 ptr[uvm.reg[b]] = uvm.reg[c];
             },
-            3 => uvm.reg[a] = uvm.reg[b] + uvm.reg[c],
-            4 => uvm.reg[a] = uvm.reg[b] * uvm.reg[c],
-            5 => uvm.reg[a] = uvm.reg[b] / uvm.reg[c],
-            6 => uvm.reg[a] = ~uvm.reg[b] & uvm.reg[c],
-            7 => return,
-            8 => { // Allocation
+            .Addition => {
+                uvm.reg[a] = uvm.reg[b] + uvm.reg[c];
+            },
+            .Multiplication => {
+                uvm.reg[a] = uvm.reg[b] * uvm.reg[c];
+            },
+            .Division => {
+                uvm.reg[a] = uvm.reg[b] / uvm.reg[c];
+            },
+            .NotAnd => {
+                uvm.reg[a] = ~uvm.reg[b] & uvm.reg[c];
+            },
+            .Halt => {
+                return;
+            },
+            .Allocate => {
                 // case 8: reg[B] = (uint)ulloc(reg[C]);
                 const memory_slot = try allocate_memory(uvm, uvm.reg[c]);
                 uvm.reg[b] = memory_slot;
             },
-            9 => { // Abandonment
+            .Abandon => {
                 const slot = uvm.reg[c];
                 const block = uvm.memory.items[slot];
                 uvm.memory.items[slot] = EMPTY_SLOT;
                 uvm.allocator.free(block);
             },
-            10 => { // Output
+            .Output => {
                 const value: u8 = @truncate(uvm.reg[c]);
                 const out = std.io.getStdOut().writer();
                 try out.print("{c}", .{value});
             },
-            11 => { // Input
+            .Input => {
                 const in = std.io.getStdIn().reader().readByte() catch 255;
                 // not sure what to do with upper bits
                 uvm.reg[c] = @intCast(in);
             },
-            12 => if (uvm.reg[b] != 0) {
-                const srcSlot = uvm.reg[b];
-                const srcBlock = uvm.memory.items[srcSlot];
-                const newSlice = try uvm.allocator.alloc(u32, srcBlock.len);
-                @memcpy(newSlice, srcBlock);
-                const originalZero = uvm.memory.items[0];
-                uvm.memory.items[0] = newSlice;
-                uvm.allocator.free(originalZero);
-                uvm.ip = uvm.reg[c];
+            .LoadProgram => {
+                if (uvm.reg[b] != 0) {
+                    const srcSlot = uvm.reg[b];
+                    const srcBlock = uvm.memory.items[srcSlot];
+                    const newSlice = try uvm.allocator.alloc(u32, srcBlock.len);
+                    @memcpy(newSlice, srcBlock);
+                    const originalZero = uvm.memory.items[0];
+                    uvm.memory.items[0] = newSlice;
+                    uvm.allocator.free(originalZero);
+                    uvm.ip = uvm.reg[c];
+                }
             },
-            13 => uvm.reg[7 & (instruction >> 25)] = instruction & 0o177777777,
-            else => unreachable,
+            .LoadConstant => {
+                uvm.reg[7 & (instruction >> 25)] = instruction & 0o177777777;
+            },
         }
     }
 }
